f46664b616fecdf113ccc2c973eebdd6f55a0653
desmond-the-developer <dumont.darville@gmail.com>
2024-08-01 15:45:43 -0400

    done

--- hospital-management/src/main/java/com/ironhack/hospital_management/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hospitalmanagement.repository;
-
-import com.example.hospitalmanagement.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
